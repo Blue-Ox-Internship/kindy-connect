a0979a4ba7be4e3e7a6d646a3fea11ea248856cb
NOBLEAHIMBISIBWE <nobleahimbisibwe5@gmail.com>
2026-07-15 10:48:01 +0300

    chore: remove unnecessary files and clean up repository

--- database/update-superadmin-id.sql
+++
@@ -1,7 +0,0 @@
--- Quick fix to update existing superadmin user ID to KC001
--- Run this if you don't want to re-seed the entire database
-
-UPDATE users SET id = 'KC001' WHERE role = 'super_admin' AND id = 'superadmin';
-
--- Update any references in audit logs
-UPDATE audit_logs SET actor_id = 'KC001' WHERE actor_id = 'superadmin';
